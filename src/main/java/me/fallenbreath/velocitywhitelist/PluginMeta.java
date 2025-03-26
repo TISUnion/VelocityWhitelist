1756fa0ae00e42b7d3aaf56915429646dac0262c
Fallen_Breath <nellaforax@outlook.com>
2025-03-27 01:40:46 +0800

    v0.3.0

--- src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
+++ src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
@@ -4,6 +4,6 @@ public class PluginMeta
 {
 	public static final String ID = "velocitywhitelist";
 	public static final String NAME = "VelocityWhitelist";
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.3.0";
 	public static final String REPOSITORY_URL = "https://github.com/TISUnion/VelocityWhitelist";
 }
